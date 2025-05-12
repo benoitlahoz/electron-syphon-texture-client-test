4de74ce64743ea5b58277426202779d15425df18
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-05-12 15:42:10 +0200

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,6 @@ import {
   ipcMain
 } from 'electron'
 import { is } from '@electron-toolkit/utils'
-import { randomUUID } from 'node:crypto'
 import path from 'node:path'
 import process from 'node:process'
 import {
@@ -16,20 +15,12 @@ import {
   SyphonServerDirectoryListenerChannel
   // @ts-ignore
 } from 'node-syphon'
-import { time } from 'node:console'
-
-export function logWithTime(message: string, ...optionalParams: any[]) {
-  const date = new Date()
-  const timestamp = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`
-  console.log(`[${timestamp}] ${message}`, ...optionalParams)
-}
-
-logWithTime(JSON.stringify(sharedTexture, null, 2))
-logWithTime('main pid: ', process.pid)
 
 let client: SyphonMetalClient | undefined
 const directory = new SyphonServerDirectory()
 directory.listen()
+
+const capturedTextures = new Map<number, any>()
 
 const createWindow = (): void => {
   const win = new BrowserWindow({
@@ -38,6 +29,7 @@ const createWindow = (): void => {
     webPreferences: {
       contextIsolation: true,
       sandbox: false,
+      // backgroundThrottling: false,
       preload: path.join(__dirname, '../preload/index.js')
     }
   })
@@ -53,128 +45,66 @@ const createWindow = (): void => {
 
   // win.webContents.openDevTools()
 
-  win.webContents.on('did-finish-load', () => {
-    logWithTime(`win pid: ${win.webContents.getOSProcessId()}`)
-  })
-
   directory.on(
     SyphonServerDirectoryListenerChannel.SyphonServerAnnounceNotification,
     (server: any) => {
       console.log('Server announced:', server)
       if (!client) {
         client = new SyphonMetalClient(server)
-        const start = Date.now()
-        /*
-        webContents.getAllWebContents().forEach((wc) => {
-          wc.send('syphon-client', client)
-        })
-        */
+
         client.on('texture', (texture: any) => {
+          console.log('Texture is null or undefined', !texture.surface)
+          // FIXME: Look like background throttling doesnt't send texture, with this error
+          // -[IOGPUMetalTexture initWithDevice:descriptor:iosurface:plane:field:args:argsSize:]:648: failed assertion `IOSurface argument must not be NULL'
+          const codedSize = {
+            width: texture.width,
+            height: texture.height
+          }
+
+          const { release } = texture
+
           const imported = sharedTexture.importSharedTexture({
-            pixelFormat: 'bgra',
-            codedSize: {
-              width: texture.width,
-              height: texture.height
-            },
+            pixelFormat: texture.pixelFormat,
+            codedSize,
             visibleRect: {
               x: 0,
               y: 0,
-              width: texture.width,
-              height: texture.height
+              ...codedSize
             },
             contentRect: {
               x: 0,
               y: 0,
-              width: texture.width,
-              height: texture.height
+              ...codedSize
             },
-            timestamp: Date.now() - start,
+            metadata: {
+              frameCount: texture.frameCount
+            },
+            timestamp: texture.timestamp,
             sharedTextureHandle: texture.surface,
             ioSurface: texture.surface
           })
           const transfer = imported.startTransferSharedTexture()
 
-          console.log(transfer)
+          capturedTextures.set(texture.frameCount, { imported, release })
 
-          const id = randomUUID()
-          // capturedTextures.set(id, { imported, texture })
-
-          // logWithTime('start send shared texture:', id)
-          win.webContents.send('shared-texture', id, transfer)
+          win.webContents.send('shared-texture', texture.frameCount, transfer)
         })
       }
-      /*
-      webContents.getAllWebContents().forEach((wc) => {
-        wc.send('syphon-server', server)
-      })
-      */
     }
   )
-  /*
-  const osr = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    show: false,
-    webPreferences: {
-      offscreen: {
-        useSharedTexture: true
-      }
-    }
-  })
-  
-  // const spout = new SpoutOutput("electron");
 
-  osr.webContents.setFrameRate(240)
-
-  osr.webContents.on('did-finish-load', () => {
-    logWithTime(`osr pid: ${osr.webContents.getOSProcessId()}`)
-    logWithTime('directory: ', directory.servers)
-  })
-
-  const capturedTextures = new Map<string, any>()
-  
   ipcMain.on('shared-texture-done', (event, id) => {
     const data = capturedTextures.get(id)
     if (data) {
-      // logWithTime('main released shared texture:', id)
-      const { imported, texture } = data
+      const { imported, release } = data
 
       imported.release(() => {
-        // logWithTime('main released source texture:', id)
-        texture.release()
+        release()
       })
 
       capturedTextures.delete(id)
     }
   })
-  
-  osr.webContents.on(
-    'paint',
-    (
-      event: Electron.WebContentsPaintEventParams,
-      dirty: Electron.Rectangle,
-      image: Electron.NativeImage
-    ) => {
-      const texture = event.texture!
-      // console.log(texture.textureInfo)
-      // @ts-ignore
-      const imported = sharedTexture.importSharedTexture({
-        ...texture.textureInfo,
-        ioSurface: texture.textureInfo.sharedTextureHandle
-      })
-
-      const transfer = imported.startTransferSharedTexture()
-
-      const id = randomUUID()
-      capturedTextures.set(id, { imported, texture })
-
-      // logWithTime('start send shared texture:', id)
-      win.webContents.send('shared-texture', id, transfer)
-    }
-  )
-
-  osr.loadURL('https://app.singular.live/output/6W76ei5ZNekKkYhe8nw5o8/Output?aspect=16:9')
-  */
 }
 
 app.whenReady().then(() => {
